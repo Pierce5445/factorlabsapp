f83aa74ed7858e429e44d99a49838a05d0d31801
Pierce <91306581+Pierce5445@users.noreply.github.com>
2023-11-12 19:32:51 -0400

    next commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,34 +1,69 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css';
 import { ConnectWallet } from '@thirdweb-dev/react';
 import Image from 'next/image';
-import {headerLogo, shoe4} from '../assets/images'
-import {hamburger} from '../assets/icons';
+import { hamburger } from '../assets/icons';
 
-export function Navbar(){
-    return(
-    
-        <header className=" padding-x py-3  z-10 w-full bg-gray-800">
-            <nav className="flex justify-between items-center">
-            <a href="https://www.factorlabs.io/" target="_blank">
-            <Image src="/images/fl_logo.png" alt="FL Logo" width={50} height={50} /></a>
-                 <p className="pl-4 text-white text-lg font-montserrat ">StegoSig</p>
-                 <ul className="flex-1 flex justify-center items-center
-                 gap-16   " >
-                    
-                    <Link href="/" className="text-white text-lg font-montserrat">Embed</Link>
-                    <Link href="/project/Verify/" className="text-white text-lg font-montserrat">Verify</Link>
-                    <Link href="" className="text-white text-lg font-montserrat">Debug Contract</Link>
+export function Navbar() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
-                 </ul>
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
 
-            <ConnectWallet
-                btnTitle='Sign In'
-                modalTitle='Select sign in method'
-            />
-            </nav>
-        </header>
+  return (
+    <header className="px-6 py-4 bg-gray-900">
+      <nav className="max container mx-auto flex justify-between items-center relative">
+        <div className="flex items-center">
+          <a href="https://www.factorlabs.io/" target="_blank" className="mr-4">
+            <Image src="/images/fl_logo.png" alt="FL Logo" width={50} height={50} />
+          </a>
+          <p className="text-white text-lg font-semibold">StegoSig</p>
+        </div>
 
-    )
-};
+        {/* Hamburger menu for mobile */}
+        <div className="lg:hidden" onClick={toggleDropdown}>
+          {/* Add your hamburger menu icon */}
+          <Image src={hamburger} alt="Hamburger Icon" width={30} height={30} />
+        </div>
+
+        {/* Navigation links for mobile */}
+        {isDropdownOpen && (
+          <div className="lg:hidden absolute top-full left-0 right-0 bg-gray-900 z-10">
+            <ul className="flex flex-col items-center mt-4 space-y-2 p-4">
+              <Link href="/" className="text-white text-lg font-semibold py-2" onClick={closeDropdown}>
+                Embed
+              </Link>
+              <Link href="/project/Verify/" className="text-white text-lg font-semibold py-2" onClick={closeDropdown}>
+                Verify
+              </Link>
+              <Link href="" className="text-white text-lg font-semibold py-2" onClick={closeDropdown}>
+                Debug Contract
+              </Link>
+            </ul>
+          </div>
+        )}
+
+        {/* Navigation links for desktop */}
+        <ul className={`lg:flex justify-center items-center space-x-6 ${isDropdownOpen ? 'hidden' : 'hidden lg:flex'}`}>
+          <Link href="/" className="text-white text-lg font-semibold">
+            Embed
+          </Link>
+          <Link href="/project/Verify/" className="text-white text-lg font-semibold">
+            Verify
+          </Link>
+          <Link href="" className="text-white text-lg font-semibold">
+            Debug Contract
+          </Link>
+        </ul>
+
+        {/* Connect wallet button */}
+        <ConnectWallet btnTitle="Sign In" modalTitle="Select sign in method" />
+      </nav>
+    </header>
+  );
+}
